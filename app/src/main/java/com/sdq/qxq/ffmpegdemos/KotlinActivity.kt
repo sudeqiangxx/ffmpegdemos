93b1e082fa85cd03dccd0be6782012c2864918fe
lennasu <lennasu@thetiger.com.cn>
2019-05-10 18:00:06 +0800

    新增循环

--- app/src/main/java/com/sdq/qxq/ffmpegdemos/KotlinActivity.kt
+++ app/src/main/java/com/sdq/qxq/ffmpegdemos/KotlinActivity.kt
@@ -78,5 +78,15 @@ class KotlinActivity : AppCompatActivity() {
         do {
             b--
         }while (b>0);
+
+
+        for (i in 1..5){
+            if (i==2){
+                continue
+            }
+            if (i==3){
+                break
+            }
+        }
     }
 }
