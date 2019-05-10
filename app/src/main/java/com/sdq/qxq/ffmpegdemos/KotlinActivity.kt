d863a8cbcdd2ef9961ddde85ebe8c4a0bd69df72
lennasu <lennasu@thetiger.com.cn>
2019-05-10 17:25:02 +0800

    添加对kotlin 循环学习

--- app/src/main/java/com/sdq/qxq/ffmpegdemos/KotlinActivity.kt
+++ app/src/main/java/com/sdq/qxq/ffmpegdemos/KotlinActivity.kt
@@ -29,5 +29,38 @@ class KotlinActivity : AppCompatActivity() {
 
             }
         }
+
+
+    }
+
+    /**
+     * 循环测试代码
+     */
+    fun testTraining(){
+        var itemss= arrayOf(11,2,4,6,7,8)
+        //itemss.indices 这个是数组的指标范围
+        for (i in itemss.indices){
+            //那就可以通过索引轮训数组
+            println(itemss[i])
+        }
+        //还可以这么搞
+        for ((index ,value) in itemss.withIndex()){
+            println("位置：$index 值：$value")
+        }
+    }
+
+    /**
+     * 集合遍历
+     */
+    fun testList(){
+        //list 初始化
+        var itemList= listOf(1,2,4,56,567,754,2,34,523,334)
+        for (item in itemList){
+            println(item)
+        }
+        //itemList.indices 有效指标集合，就是我们集合下标的集合，我们可以进行轮训
+        for (index in itemList.indices){
+            println("集合下标：$index 对应的值：${itemList[index]}")
+        }
     }
 }
