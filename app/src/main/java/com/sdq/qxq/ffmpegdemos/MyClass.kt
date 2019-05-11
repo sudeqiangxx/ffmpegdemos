024de392c4fc8962b49df8cc3af0726f28a0871e
lennasu <1320909689@qq.com>
2019-05-11 14:21:34 +0800

    接口实现

---
+++ app/src/main/java/com/sdq/qxq/ffmpegdemos/MyClass.kt
@@ -0,0 +1,14 @@
+package com.sdq.qxq.ffmpegdemos
+
+/**
+ * @description:
+ * @author: lenna
+ * @date: 2019/5/11
+ * @update: 2019/5/11
+ * @version: 1.0
+ */
+class MyClass :MyInterface {
+    override fun bar() {
+        print("接口实现")
+    }
+}
---
+++ app/src/main/java/com/sdq/qxq/ffmpegdemos/MyInterface.kt
@@ -0,0 +1,15 @@
+package com.sdq.qxq.ffmpegdemos
+
+/**
+ * @description:
+ * @author: lenna
+ * @date: 2019/5/11
+ * @update: 2019/5/11
+ * @version: 1.0
+ */
+interface MyInterface {
+    fun bar()
+    fun foof(){
+        print("我是接口方法")
+    }
+}
